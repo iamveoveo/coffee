6025efe780faf3f96ed8f4c578dda020ab6355b2
iamveoveo <vinhveoveo21@gmail.com>
2022-05-30 20:05:47 +0700

    training done

--- extract_data.py
+++ extract_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 raw_data = pd.read_csv('coffee-quality-database\\data\\arabica_data_cleaned.csv', index_col = 0)
 
-features = ['Aroma', 'Flavor', 'Aftertaste', 'Acidity', 'Body', 'Balance', 'Uniformity', 'Clean.Cup', 'Sweetness', 'Cupper.Points', 'Total.Cup.Points', 'Moisture', 'Quakers', ]
+features = ['Aroma', 'Flavor', 'Aftertaste', 'Acidity', 'Body', 'Balance', 'Uniformity', 'Clean.Cup', 'Sweetness', 'Cupper.Points', 'Total.Cup.Points', ]
 data = pd.DataFrame(raw_data, columns=features)
 
 file_name = 'coffee-quality-database\\data\\arabica_data_extracted.csv'
